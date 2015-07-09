1c73ae8a9932f2c8de42d701e0181cdba2bed86d
Dennis Wethmar <denniswethmar@gmail.com>
2015-07-09 14:24:26 +0200

    Split up the selector and css classes.

--- src/Unslider.php
+++ src/Unslider.php
@@ -41,24 +41,49 @@ class Unslider extends Widget {
 
     public $use_default_styling = true;
 
+    /**
+     * @var string selector used for jquery
+     */
     public $selector = 'banner';
 
+    /**
+     * @var array available css classes
+     */
+    public $css_container_classes = ['banner'];
+
+    /**
+     * @var array available css classes
+     */
+    public $css_button_classes = ['btn'];
+
+    /**
+     * @var array slide data. See this class description for an example.
+     */
     public $slides = [];
 
+    /**
+     * @var array options passed to the unslider jquery plugin.
+     */
     public $options = [
         'dots' => true,
         'keys' => false,
         'fluid' => true
     ];
 
+    /**
+     * @inheritdoc
+     */
     public function init() {
         parent::init();
     }
 
+    /**
+     * @inheritdoc
+     */
     public function run() {
         $this->registerAssets();
 
-        $slider = Html::beginTag('div', ['class' => $this->selector]);
+        $slider = Html::beginTag('div', ['class' => $this->getContainerCssClasses()]);
         $slider .= Html::beginTag('ul');
         foreach ($this->slides as $slide) {
             $slider .= '<li style="background-image: url(' . $slide['img'] . ')">';
@@ -86,5 +111,39 @@ class Unslider extends Widget {
         $view->registerJs("jQuery('.$this->selector').unslider($options);");
     }
 
-}
+    /**
+     * Get classes for the container element
+     *
+     * @return string container css classes
+     */
+    protected function getContainerCssClasses() {
+        return $this->_flattenCssClasses($this->css_container_classes);
+    }
 
+    /**
+     * Get classes for the button element
+     *
+     * @return string button css classes
+     */
+    protected function getButtonCssClasses() {
+        return $this->_flattenCssClasses($this->css_container_classes);
+    }
+
+    /**
+     * Flatten an array with implode. If the passed $css_classes is a string
+     * then we return the param as is.
+     * 
+     * @param array|string $css_classes the provided css classes
+     * @return string the flattend css classes
+     */
+    private function _flattenCssClasses($css_classes) {
+        $flatend_css_classes = '';
+        if(is_array($css_classes) && empty($css_classes)) {
+            $flatend_css_classes = implode(' ', $css_classes);
+        } else if (is_string($css_classes)) {
+            $flatend_css_classes = $css_classes;
+        }
+        return $flatend_css_classes;
+    }
+
+}
